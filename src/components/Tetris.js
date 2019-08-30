d50e15c0de79e7a1f7cc8ade7eb7e242cc93192a
Samitha Padukkage Don <samitha.312@gmail.com>
2019-08-30 15:55:25 +1000

    Added more stage logic

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -69,7 +69,7 @@ const Tetris = () => {
                             <Display text="Level" />
                         </div>
                     )}
-                    <StartButton onClick={startGame} />
+                    <StartButton callback={startGame} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
