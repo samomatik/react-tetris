c25bc290930e2cfd7e84b80e6b98c2b332707fab
Samitha Padukkage Don <samitha.312@gmail.com>
2019-08-30 16:33:45 +1000

    Fixed player reset

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -19,7 +19,7 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [stage, setStage] = useStage(player, resetPlayer);
 
     console.log('re-render');
 
