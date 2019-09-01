f25790b69909949e76df12135ac2f28c468f93f0
Samitha Padukkage Don <sam@samomtik.com>
2019-09-01 13:23:25 +1000

    Updates

---
+++ precache-manifest.c6e04e94d432aaa63fb4064e7d796fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4a6ca06ea3ee41fa1627f3f31696aa",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "bb76ebe4b21b09e9dce0",
+    "url": "/react-tetris/static/css/main.13370a51.chunk.css"
+  },
+  {
+    "revision": "acd81646e67dd47cc3d5",
+    "url": "/react-tetris/static/js/2.60de6f26.chunk.js"
+  },
+  {
+    "revision": "bb76ebe4b21b09e9dce0",
+    "url": "/react-tetris/static/js/main.cc780c3d.chunk.js"
+  },
+  {
+    "revision": "569332aecf4ad6640d6a",
+    "url": "/react-tetris/static/js/runtime~main.43a5b001.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
